a8e0bef652678f32efb36a0dac76086d0742cbe0
kittenchilly <mario.pikmin.zelda@gmail.com>
2022-03-24 12:27:00 -0500

    Add some more stuff

--- main.lua
+++ main.lua
@@ -5,8 +5,19 @@ CollectibleType.COLLECTIBLE_ANCIENT_REVELATION = Isaac.GetItemIdByName("Ancient 
 
 function mod:EvaluateCache(player, cacheFlag)
 	if player:HasCollectible(CollectibleType.COLLECTIBLE_ANCIENT_REVELATION) then
-		if cacheFlag == CacheFlag.CACHE_FLYING then
+		if cacheFlag == CacheFlag.CACHE_DAMAGE then
+		
+		elseif cacheFlag == CacheFlag.CACHE_FIREDELAY then
+		
+		elseif cacheFlag == CacheFlag.CACHE_SHOTSPEED then
+			player.ShotSpeed = player.ShotSpeed + 0.38
+		elseif cacheFlag == CacheFlag.CACHE_FLYING then
 			player.CanFly = true
+		elseif cacheFlag == CacheFlag.CACHE_TEARCOLOR then
+		
+		elseif cacheFlag == CacheFlag.CACHE_TEARFLAG then
+			player.TearFlags = player.TearFlags | TearFlags.TEAR_SPECTRAL
+			player.TearFlags = player.TearFlags | TearFlags.TEAR_TURN_HORIZONTAL
 		end
 	end
 end
