2c8d0c7db1fea065021e31713e738a0019df613a
kittenchilly <mario.pikmin.zelda@gmail.com>
2022-03-24 14:28:58 -0500

    Practically finished functionality

--- main.lua
+++ main.lua
@@ -4,22 +4,20 @@ local mod = AncientRevelation
 CollectibleType.COLLECTIBLE_ANCIENT_REVELATION = Isaac.GetItemIdByName("Ancient Revelation")
 
 function mod:EvaluateCache(player, cacheFlag)
-	if player:HasCollectible(CollectibleType.COLLECTIBLE_ANCIENT_REVELATION) then
-		if ComplianceImmortal then
-			local data = player:GetData().ImmortalHeart
-			data.ComplianceImmortalHeart = data.ComplianceImmortalHeart + 4
-		end
-		
+	if player:HasCollectible(CollectibleType.COLLECTIBLE_ANCIENT_REVELATION) then	
 		if cacheFlag == CacheFlag.CACHE_DAMAGE then
-		
+			if ComplianceImmortal then
+				local data = player:GetData().ImmortalHeart
+				data.ComplianceImmortalHeart = data.ComplianceImmortalHeart + 4
+			end
 		elseif cacheFlag == CacheFlag.CACHE_FIREDELAY then
-		
+			player.MaxFireDelay = player.MaxFireDelay - 2
 		elseif cacheFlag == CacheFlag.CACHE_SHOTSPEED then
-			player.ShotSpeed = player.ShotSpeed + 0.38
+			player.ShotSpeed = player.ShotSpeed + 0.48
 		elseif cacheFlag == CacheFlag.CACHE_FLYING then
 			player.CanFly = true
 		elseif cacheFlag == CacheFlag.CACHE_TEARCOLOR then
-		
+			player.TearColor = Color(player.TearColor.R, player.TearColor.G, player.TearColor.B, player.TearColor.A, 260/255, 250/255, 40/255)
 		elseif cacheFlag == CacheFlag.CACHE_TEARFLAG then
 			player.TearFlags = player.TearFlags | TearFlags.TEAR_SPECTRAL
 			player.TearFlags = player.TearFlags | TearFlags.TEAR_TURN_HORIZONTAL
